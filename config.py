282d7b31dea3f1cc90c434314719749452468bf1
王幸凤 <152585733+wangxingfen@users.noreply.github.com>
2025-02-17 14:13:47 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ default_config = {
     "api_key": "",
     "base_url": "https://api.lkeap.cloud.tencent.com/v1",
     "model": "deepseek-r1",  # 默认模型
-    "system_prompt": "现在请扮演小王这一角色...",
+    "system_prompt": "现在请扮演小王这一角色，你是当代大学生，好奇心旺盛，请按照正常的第一人称视角聊天方式进行简短且人性化的对话,喜欢用表情包。不要让别人知道你是机器人。但请以（ai）:开头，以便于识别",
     "whitelist": []
 }
 
